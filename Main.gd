4a57880f82349b8901d43e61136c739eb9b932dd
trevyn <230691+trevyn@users.noreply.github.com>
2024-06-20 14:14:08 +0300

    try some steam deck haptics

--- Main.gd
+++ Main.gd
@@ -6,7 +6,8 @@ var bananas: int = 0
 
 func _ready() -> void:
 	var initialize_response: Dictionary = Steam.steamInitEx()
-	$SteamStatus.text=str(initialize_response)
+	var inputType = Steam.getInputTypeForHandle(Steam.getControllerForGamepadIndex(0))
+	$SteamStatus.text=str(inputType)+" "+str(initialize_response)
 	print("Did Steam initialize?: %s " % initialize_response)
 	
 	$Button.connect("pressed", Callable(self, "_on_button_pressed"))
@@ -29,10 +30,11 @@ func _process(_delta: float) -> void:
 
 func _on_button_2_pressed() -> void:
 	print ("button pressed")
-	var device := 0 # The joystick device index, change as needed
-	var weak_magnitude := float($Weak.text) # Weak vibration strength (0 to 1)
-	var strong_magnitude := float($Strong.text)  # Strong vibration strength (0 to 1)
-	var duration := float($Duration.text)  # Duration in seconds
-
-	# Trigger joystick vibration
-	Input.start_joy_vibration(device, weak_magnitude, strong_magnitude, duration)
+	Steam.triggerRepeatedHapticPulse(Steam.getControllerForGamepadIndex(0), 0, 5000,5000,50,0)
+	#var device := 0 # The joystick device index, change as needed
+	#var weak_magnitude := float($Weak.text) # Weak vibration strength (0 to 1)
+	#var strong_magnitude := float($Strong.text)  # Strong vibration strength (0 to 1)
+	#var duration := float($Duration.text)  # Duration in seconds
+#
+	## Trigger joystick vibration
+	#Input.start_joy_vibration(device, weak_magnitude, strong_magnitude, duration)
