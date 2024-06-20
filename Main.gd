af4a557e92cbad1150cc8df67656f884e9267a7b
trevyn <230691+trevyn@users.noreply.github.com>
2024-06-20 13:23:54 +0300

    add vibration

--- Main.gd
+++ Main.gd
@@ -15,9 +15,20 @@ func _on_button_pressed():
 	var instance = falling_object_scene.instantiate()
 	
 	instance.position = Vector2(randf_range(0, get_viewport_rect().size.x), 0)
-			
+	
 	add_child(instance)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+
+
+func _on_button_2_pressed() -> void:
+	print ("button pressed")
+	var device := 0 # The joystick device index, change as needed
+	var weak_magnitude := float($Weak.text) # Weak vibration strength (0 to 1)
+	var strong_magnitude := float($Strong.text)  # Strong vibration strength (0 to 1)
+	var duration := float($Duration.text)  # Duration in seconds
+
+	# Trigger joystick vibration
+	Input.start_joy_vibration(device, weak_magnitude, strong_magnitude, duration)
