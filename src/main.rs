fbd689dcb93065f7f328b92bcedf57e3354136eb
trevyn <230691+trevyn@users.noreply.github.com>
2024-08-08 18:40:55 +0400

    add steamworks

--- src/main.rs
+++ src/main.rs
@@ -44,5 +44,35 @@ fn config() -> GlassConfig {
 fn main() -> Result<(), GlassError> {
     eprintln!("Hello world!");
 
+    match steamworks::Client::init() {
+        Ok(client) => {
+            let _cb = client.register_callback(|p: steamworks::PersonaStateChange| {
+                println!("Got callback: {:?}", p);
+            });
+
+            let utils = client.utils();
+            println!("Utils:");
+            println!("AppId: {:?}", utils.app_id());
+            println!("UI Language: {}", utils.ui_language());
+
+            let apps = client.apps();
+            println!("Apps");
+            println!(
+                "IsInstalled(480): {}",
+                apps.is_app_installed(steamworks::AppId(480))
+            );
+            println!(
+                "InstallDir(480): {}",
+                apps.app_install_dir(steamworks::AppId(480))
+            );
+            println!("BuildId: {}", apps.app_build_id());
+            println!("AppOwner: {:?}", apps.app_owner());
+            println!("Langs: {:?}", apps.available_game_languages());
+            println!("Lang: {}", apps.current_game_language());
+        }
+
+        Err(e) => println!("Steam client error: {}", e),
+    }
+
     Glass::run(config(), |context| Box::new(FluidSimApp::new(context)))
 }
